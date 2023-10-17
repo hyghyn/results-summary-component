c942789463a9a9c7f083b87580678c908a50bc67
HYGHYN <hyghyn@hotmail.com>
2023-10-17 22:26:16 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,22 @@ export default function App() {
       {
         category: "Reaction",
         score: 80,
-        icon: "./src/assets/icon-reaction.svg",
+        icon: "./assets/icon-reaction.svg",
       },
       {
         category: "Memory",
         score: 92,
-        icon: "./src/assets/icon-memory.svg",
+        icon: "./assets/icon-memory.svg",
       },
       {
         category: "Verbal",
         score: 61,
-        icon: "./src/assets/icon-verbal.svg",
+        icon: "./assets/icon-verbal.svg",
       },
       {
         category: "Visual",
         score: 72,
-        icon: "./src/assets/icon-visual.svg",
+        icon: "./assets/icon-visual.svg",
       },
     ]);
   }, []);
