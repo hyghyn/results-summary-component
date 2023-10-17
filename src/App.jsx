cf3996beb5942faf882fac458b15c97ab7a679be
HYGHYN <hyghyn@hotmail.com>
2023-10-17 22:29:17 +0700

    move and update

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,22 @@ export default function App() {
       {
         category: "Reaction",
         score: 80,
-        icon: "./assets/icon-reaction.svg",
+        icon: "icon-reaction.svg",
       },
       {
         category: "Memory",
         score: 92,
-        icon: "./assets/icon-memory.svg",
+        icon: "icon-memory.svg",
       },
       {
         category: "Verbal",
         score: 61,
-        icon: "./assets/icon-verbal.svg",
+        icon: "icon-verbal.svg",
       },
       {
         category: "Visual",
         score: 72,
-        icon: "./assets/icon-visual.svg",
+        icon: "icon-visual.svg",
       },
     ]);
   }, []);
